8bcedd27fd60456e0b469ed059aca8fb7e8349c9
FangFeiyue <18766131540@163.com>
2022-01-04 17:30:05 +0800

    初始化项目

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import 'assets/style/index.scss';
+import 'utils/rem.js';
 
-createApp(App).mount('#app')
+createApp(App).mount('#app');
