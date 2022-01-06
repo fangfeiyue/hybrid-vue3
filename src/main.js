ee2f3e1f61a1a0a7d8a966ef71a528c645348784
FangFeiyue <18766131540@163.com>
2022-01-06 16:47:39 +0800

    引入VueRouter

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import router from './router/index';
 import 'assets/style/index.scss';
 import 'utils/rem.js';
 
-createApp(App).mount('#app');
+const app = createApp(App);
+
+app.use(router).mount('#app');
