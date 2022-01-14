b480ae29dfbcc7e4f8a7717d2b15b325dbe676ff
FangFeiyue <18766131540@163.com>
2022-01-14 16:13:54 +0800

    设置轮播图分页器颜色

--- src/components/Swiper.vue
+++ src/components/Swiper.vue
@@ -1,54 +1,34 @@
 <script setup>
 import { Swiper, SwiperSlide } from "swiper/vue";
-import { Pagination } from "swiper";
+import { Pagination, Autoplay } from "swiper";
 import "swiper/css";
 import "swiper/css/pagination";
 const props = defineProps(["swiperImgs", "height"]);
 const onSwiper = (swiper) => {};
 const onSlideChange = () => {};
-const swiperOptions = {
-  autoplay: true,
-  loop: true,
-  pagination: {
-    el: "swiper-pagination",
-    clickable: true,
-    bulletClass: "custom-bullet-class",
-  },
-};
+
 const pagination = {
-  el: "swiper-pagination",
-  bulletClass: "custom-bullet-class",
+  clickable: true,
 };
 </script>
 <template>
-  <swiper
-    :modules="[Pagination]"
+  <Swiper
+    :modules="[Pagination, Autoplay]"
     :loop="true"
     :autoplay="true"
-    :pagination="{ clickable: true }"
+    :pagination="pagination"
     @swiper="onSwiper"
     @slideChange="onSlideChange"
   >
     <swiper-slide v-for="item in swiperImgs" :key="item"
       ><img :style="{ height: height }" :src="item.icon"
     /></swiper-slide>
-  </swiper>
+  </Swiper>
 </template>
 <style lang="scss" scoped>
 @import "assets/style/rem.scss";
-.swiper-pagination {
-  .custom-bullet-class {
-    box-sizing: border-box;
-    border-radius: 100%;
-    height: px2rem(6);
-    width: px2rem(6);
-    border: px2rem(1) solid #fff;
-    margin: 0 px2rem(4);
-    display: inline-block;
-    opacity: 1;
-  }
-  .swiper-pagination-bullet-active {
-    background-color: #fff;
-  }
+.swiper {
+  --swiper-theme-color: #fff;
+  // --swiper-pagination-color: #00ff33; /* 两种都可以 */
 }
 </style>
