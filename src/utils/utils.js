c3acde397a86d0f1b8b662890d70e60f07a6f0fe
FangFeiyue <18766131540@163.com>
2022-01-17 18:16:11 +0800

    添加补零工具函数

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,9 +1,15 @@
-export const getImgPath = (name) => {
+export function getImgPath(name) {
 	return new URL('../assets/img/' + name, import.meta.url).href;
-};
+}
 
-export const formatePrice = (price) => {
+export function formatePrice(price) {
 	price = +price;
 	if (isNaN(price)) price = 0.0;
 	return price.toFixed(2);
-};
+}
+
+export function fillZero(val) {
+	val = Number(val);
+	if (isNaN(val)) val = 0;
+	return val < 9 ? '0' + val : val;
+}
