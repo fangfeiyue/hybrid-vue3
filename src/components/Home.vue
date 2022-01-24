32f11eaf56cb04394d7d03adff5feff20d64e76d
FangFeiyue <18766131540@163.com>
2022-01-24 16:58:53 +0800

    NavBar组件的开发

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,15 @@
 <script setup>
 import { fetchSwiper, fetchActivity, fetchSeconds } from "../api/index";
+import NavBar from "./NavBar.vue";
 import Swiper from "./Swiper.vue";
 import Activity from "./Activity.vue";
 import Seconds from "./Seconds.vue";
 import Goods from "./Goods.vue";
 import ModeOptions from "./ModeOptions.vue";
+import Search from "./Search.vue";
 import { ref } from "@vue/reactivity";
 import { onMounted } from "@vue/runtime-core";
+import { getImgPath, throttle } from "../utils/utils";
 
 // const getImgPath = (name) => {
 //   return new URL("../assets/img/" + name, import.meta.url).href;
@@ -39,10 +42,71 @@ onMounted(() => {
   getActivity();
   getSeconds();
 });
+
+const criticalValue = 160;
+const navBarStyle = ref({
+  backgroundColor: "",
+  position: "fixed",
+});
+const navBarData = {
+  normal: {
+    leftIcon: getImgPath("more-white.svg"),
+    rightIcon: getImgPath("message-white.svg"),
+    searchStyle: {
+      bgColor: "#ffffff",
+      hintColor: "#999999",
+      icon: getImgPath("search.svg"),
+    },
+  },
+  highlight: {
+    leftIcon: getImgPath("more.svg"),
+    rightIcon: getImgPath("message.svg"),
+    searchStyle: {
+      bgColor: "#d7d7d7",
+      hintColor: "#ffffff",
+      icon: getImgPath("search-white.svg"),
+    },
+  },
+};
+const navBarCurStyle = ref({});
+onMounted(() => {
+  navBarCurStyle.value = navBarData.normal;
+});
+const calNavBarStyle = (e) => {
+  const scrollTop = e.target.scrollTop;
+  const opacity = scrollTop / criticalValue;
+  if (scrollTop >= criticalValue) {
+    navBarCurStyle.value = navBarData.highlight;
+    navBarStyle.value = {
+      ...navBarStyle.value,
+      backgroundColor: `rgba(255, 255, 255, ${opacity})`,
+    };
+  } else {
+    navBarCurStyle.value = navBarData.normal;
+    navBarStyle.value = {
+      ...navBarStyle.value,
+      backgroundColor: `rgba(255, 255, 255, ${opacity})`,
+    };
+  }
+};
+const onScroll = throttle(calNavBarStyle, 200);
 </script>
 <template>
-  <div class="home">
+  <div class="home" @scroll="onScroll">
     <div class="home-content">
+      <nav-bar :isShowBack="false" :navBarStyle="navBarStyle">
+        <template v-slot:nav-left>
+          <img :src="navBarCurStyle.leftIcon" alt="更多" />
+          <!-- <img src="../assets/img/more-white.svg" /> -->
+        </template>
+
+        <template v-slot:nav-center>
+          <Search v-bind="navBarCurStyle.searchStyle" />
+        </template>
+        <template v-slot:nav-right>
+          <img :src="navBarCurStyle.rightIcon" alt="信息" />
+        </template>
+      </nav-bar>
       <Swiper :swiperImgs="swiperImgs" height="184px" />
       <Activity>
         <div class="activity-item">
---
+++ src/components/NavBar.vue
@@ -0,0 +1,62 @@
+<script setup>
+import Search from "./Search.vue";
+defineProps(["titleName", "isShowBack", "navBarStyle"]);
+</script>
+<template>
+  <div
+    class="nav-bar"
+    :class="[{ 'bottom-line': titleName }]"
+    :style="navBarStyle"
+  >
+    <div class="left">
+      <!-- <img src="../assets/img/more-white.svg" /> -->
+      <slot name="nav-left">
+        <img v-if="isShowBack" src="../assets/img/back.svg" />
+      </slot>
+    </div>
+    <div class="center">
+      <slot name="nav-center">
+        <span class="nav-title">{{ titleName }}</span>
+      </slot>
+    </div>
+    <div class="right">
+      <slot name="nav-right"></slot>
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+@import "assets/style/rem.scss";
+@import "assets/style/color.scss";
+.nav-bar {
+  width: 100%;
+  height: $navBarHeight;
+  line-height: $navBarHeight;
+  display: flex;
+  justify-content: space-between;
+  z-index: 999;
+
+  .left,
+  .right {
+    display: flex;
+    height: 100%;
+    width: px2rem(26);
+    padding: 0 px2rem(8);
+    ::v-deep img {
+      width: 100%;
+    }
+  }
+  .center {
+    display: flex;
+    height: 100%;
+    flex-grow: 1;
+    .nav-title {
+      align-self: center;
+      margin: 0 auto;
+      font-size: $titleSize;
+    }
+  }
+}
+.bottom-line {
+  border-bottom: 1px solid $lineColor;
+}
+</style>
